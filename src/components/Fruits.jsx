3e44e6befb206e297955481e0a96ef3b00eee8f7
mythily-muthu <mythilimuthu18@gmail.com>
2023-11-22 14:37:48 +0530

    Feat: added edit button

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -6,23 +6,24 @@ const Fruits = ({ fruits }) => {
 
   const handleDelete = (fruitToDelete) => {
     let remainingFruits = allFruits.filter((fruit) => fruit !== fruitToDelete);
-
     setAllFruits(remainingFruits);
   };
+
+  const handleEdit = () => {};
 
   useEffect(() => {
     setAllFruits(fruits);
   }, [fruits]);
 
   return (
-    <div className="flex h-max bg-blue-100 border w-64 ">
+    <div className="flex h-max bg-blue-100 border w-80 ">
       <div className="flex flex-col gap-3 p-10 w-full">
         {allFruits.map((fruit, index) => (
           <div
             className="flex justify-between w-full hover:bg-white"
             key={index}
           >
-            <p className="font-bold items-start hover:cursor-pointer">
+            <p className="font-bold items-start hover:cursor-pointer ">
               {fruit}
             </p>
             <p
@@ -30,6 +31,12 @@ const Fruits = ({ fruits }) => {
               className="font-bold text-red-700 hover:cursor-pointer hover:text-red-500"
             >
               D
+            </p>
+            <p
+              onClick={() => handleEdit(fruit)}
+              className="font-bold text-red-700 hover:cursor-pointer hover:text-red-500"
+            >
+              E
             </p>
           </div>
         ))}
--- src/constants/index.js
+++ src/constants/index.js
@@ -6,3 +6,30 @@ export let fruits = [
   "Banana",
   "Watermelon",
 ];
+
+// export let fruits = [
+//   {
+//     name: "Apple",
+//     count: 0,
+//   },
+//   {
+//     name: "Mango",
+//     count: 0,
+//   },
+//   {
+//     name: "Guava",
+//     count: 0,
+//   },
+//   {
+//     name: "Pomegranate",
+//     count: 0,
+//   },
+//   {
+//     name: "Banana",
+//     count: 0,
+//   },
+//   {
+//     name: "Watermelon",
+//     count: 0,
+//   },
+// ];
